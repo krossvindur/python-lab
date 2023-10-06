4e1273192eefcf1f1db40ee8c51260b0b4014c4e
risttuules <krossvindur@gmail.com>
2023-10-06 16:04:15 -0300

    removing verbosity

--- exercises/d7nd5.py
+++ exercises/d7nd5.py
@@ -1,13 +1,3 @@
-# whenever applicable, all excercises must always pay 
-# special attention to:
-# 1. is there a faster way? (execution speed)
-# 2. is there a simpler way? (code size and readability)
-# 3. is there a lighter way? (memory footprint)
-
-# Write a program which will find all such numbers which are divisible
-# by 7 but are not a multiple of 5, between n and m (both
-# included).
-
 def d7nd5(start, end):
 
     if start%7 != 0:
